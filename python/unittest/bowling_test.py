0f400a55dbdc4ba476b8824b46ee8679607c55cf
amith <amithnair91@gmail.com>
2023-02-08 19:42:59 +0400

    pass test

--- python/unittest/bowling_test.py
+++ python/unittest/bowling_test.py
@@ -12,6 +12,14 @@ def score():
     return _pins
 
 
+class Game:
+    def roll(self, pins):
+        roll(pins)
+
+    def score(self):
+        return score()
+
+
 class BowlingTestCase(unittest.TestCase):
 
     def test_score_is_zero_when_zero_pins_are_knocked(self):
@@ -19,5 +27,6 @@ class BowlingTestCase(unittest.TestCase):
         self.assertEqual(0, score())
 
     def test_score_is_one_when_one_pin_is_knocked(self):
-        roll(1)
-        self.assertEqual(1, score())
+        game = Game()
+        game.roll(1)
+        self.assertEqual(1, game.score())
