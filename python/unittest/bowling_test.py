4ba845382e40d9360626522c8dea4480ec348206
amith <amithnair91@gmail.com>
2023-02-08 19:34:25 +0400

    fail test

--- python/unittest/bowling_test.py
+++ python/unittest/bowling_test.py
@@ -14,3 +14,7 @@ class BowlingTestCase(unittest.TestCase):
     def test_score_is_zero_when_zero_pins_are_knocked(self):
         roll(0)
         self.assertEqual(score(), 0)
+
+    def test_score_is_zero_when_one_pin_is_knocked(self):
+        roll(1)
+        self.assertEqual(score(), 1)
