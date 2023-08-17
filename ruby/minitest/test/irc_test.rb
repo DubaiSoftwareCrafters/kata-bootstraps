4056fde904a76294959d5ff019c9ad3525280ff9
amith <amithnair91@gmail.com>
2023-08-17 19:19:42 +0400

    failing test

--- ruby/minitest/test/irc_test.rb
+++ ruby/minitest/test/irc_test.rb
@@ -2,8 +2,16 @@ require 'minitest/autorun'
 
 class IrcTest < Minitest::Test
 
-  def test_do_something
-
+  def test_connect
+    client = IrcClient.new
+    assert client.is_connected
   end
 
+end
+
+class IrcClient
+
+  def is_connected
+    # code here
+  end
 end
