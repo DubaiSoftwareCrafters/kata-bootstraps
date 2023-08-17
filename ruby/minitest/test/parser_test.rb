3c5282cddaa8c3afca605342dbbd0a6fdd7a3d3f
amith <amithnair91@gmail.com>
2023-08-17 20:34:38 +0400

    pass test

--- ruby/minitest/test/parser_test.rb
+++ ruby/minitest/test/parser_test.rb
@@ -4,13 +4,13 @@ require 'minitest/autorun'
 class ParserTest < Minitest::Test
 
   def parse_channel(line)
-
+    line.partition('#').last.split(" ").first
   end
 
   def test_channel_name
     line = ":lithium.libera.chat 322 CCClient #yabause 12 :Yabause, A Beautiful And Under-rated Saturn Emulator | 0.9.15 has been released! | This is COOL | Our new home!"
 
-    assert parse_channel(line) == "#yabause"
+    assert_equal  "yabause", parse_channel(line)
   end
 
 end
