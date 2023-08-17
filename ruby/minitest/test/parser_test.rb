83aefcc46a776bb6d6b1600205efa681e77265da
amith <amithnair91@gmail.com>
2023-08-17 20:36:31 +0400

    failing test

--- ruby/minitest/test/parser_test.rb
+++ ruby/minitest/test/parser_test.rb
@@ -11,6 +11,18 @@ class ParserTest < Minitest::Test
     line = ":lithium.libera.chat 322 CCClient #yabause 12 :Yabause, A Beautiful And Under-rated Saturn Emulator | 0.9.15 has been released! | This is COOL | Our new home!"
 
     assert_equal  "yabause", parse_channel(line)
+    end
+
+  def parse_channels(lines1)
+    # code here
+  end
+
+  def test_channel_names
+    line = ":lithium.libera.chat 322 CCClient #yabause 12 :Yabause, A Beautiful And Under-rated Saturn Emulator | 0.9.15 has been released! | This is COOL | Our new home!"
+
+    lines = [line,line]
+
+    assert_equal  ["yabause","yabause"], parse_channels(lines)
   end
 
 end
