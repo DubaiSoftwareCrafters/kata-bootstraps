5d6d4ca915528e975dd21f834975e5421258ad6b
amith <amithnair91@gmail.com>
2023-08-17 19:23:39 +0400

    pass test

--- ruby/minitest/test/irc_test.rb
+++ ruby/minitest/test/irc_test.rb
@@ -3,15 +3,23 @@ require 'minitest/autorun'
 class IrcTest < Minitest::Test
 
   def test_connect_invalid_host
-    client = IrcClient.new
-    assert client.is_connected
+    client = IrcClient.new('invalid host')
+    refute client.is_connected
   end
 
 end
 
 class IrcClient
 
+  def initialize(host)
+    begin
+    TCPSocket.new(host,8888)
+    rescue NameError
+      @is_connected = false
+    end
+  end
+
   def is_connected
-    # code here
+    @is_connected
   end
 end
